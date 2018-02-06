d3c32095bfd41713ae864e6114981d54ea1f8a05
David Rodenas <david.rodenas@gmail.com>
2018-02-06 16:30:53 +0100

    project creation

---
+++ src/test/java/com/drpicox/dddshop/DddShopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.drpicox.dddshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DddShopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
